bef578134983639deccffc629425aeb450ad7b95
k <k@klabz.org>
2025-08-04 20:19:47 +0200

    Fix PR #1274

--- h2d/Text.hx
+++ h2d/Text.hx
@@ -383,7 +383,7 @@ class Text extends Drawable {
 				}
 				else wLastSep = size;
 			}
-			else if( wordWrap && (x + esize + letterSpacing) - wLastSep > (maxWidth-esize) ) {
+			else if( wordWrap && (x + esize + letterSpacing) - wLastSep > maxWidth ) {
 				newline = true;
 				lines.push(text.substr(restPos, i - restPos));
 				restPos = i;
